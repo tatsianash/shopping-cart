e1e540bcb374d43da4e0b17514a61d749a3d88f6
Tatsiana Shapavalava <tatsiana_shapavalava1@epam.com>
2023-01-25 09:30:37 -0700

    Iitialize React +TS +tailwind project

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import './App.css';
+
+function App() {
+  return <div className="font-bold text-red-800">set up vite +react</div>;
+}
+
+export default App;
