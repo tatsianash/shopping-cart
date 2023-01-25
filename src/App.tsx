d240767f3123de71fca84f409050f1a2fa4ae66d
Tatsiana Shapavalava <tatsiana_shapavalava1@epam.com>
2023-01-25 11:04:00 -0700

    Add nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Store from './pages/Store';
+import About from './pages/About';
+import Navbar from './components/Navbar';
+
+// import './App.css';
 
 function App() {
-  return <div className="font-bold text-red-800">set up vite +react</div>;
+  return (
+    <>
+      <Navbar />
+      <div className="container mx-auto mb-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/Store.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Store() {
+  return <h1>Store</h1>;
+}
