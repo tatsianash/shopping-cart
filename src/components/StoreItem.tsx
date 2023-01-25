13e4b2fa4678d06056bac8cdc9163cab4d3533fa
Tatsiana Shapavalava <tatsiana_shapavalava1@epam.com>
2023-01-25 13:26:45 -0700

    Add Card item element

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -7,5 +7,20 @@ interface IProps {
   imgUrl: string;
 }
 export default function StoreItem({ id, name, price, imgUrl }: IProps) {
-  return <div>{name}</div>;
+  return (
+    <div className="max-w-sm rounded-md shadow-xl">
+      <img
+        src={imgUrl}
+        alt={name}
+        className="w-full object-cover overflow-hidden h-52"
+      />
+      <div className="flex flex-col p-4">
+        <div className="container flex justify-between">
+          <div>{name}</div>
+          <div>${price}</div>
+        </div>
+        <div> add to cart</div>
+      </div>
+    </div>
+  );
 }
